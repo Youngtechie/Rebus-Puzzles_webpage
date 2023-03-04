700e66704ec6c6eeacde7110bab9d7ca88d2e216
Youngtechie <olaegbeolamilekan1@gmail.com>
2023-03-04 09:02:19 +0100

    added one rebus

--- src/Rebuses.jsx
+++ src/Rebuses.jsx
@@ -1,94 +1,99 @@
+const rebuses = [
+  {
+    pattern: `get + get + get + get`,
+    answer: " forget ",
+    hint: "A verb.",
+  },
+  //*****************************************************
+  // Add your rebuses here
 
-const rebuses = [{
-  pattern:`get + get + get + get`,
-  answer:" forget ",
-  hint: "A verb.",
-},
-//*****************************************************
-// Add your rebuses here
-
-{
-  pattern: `improp + 2`,
-  answer: `impromptu`,
-  hint: `unprepared`,
-},
-{
-  pattern: `&#128514 + gas`,
-  answer:" laughing gas ",
-  hint: "A popular name for 'Nitrous Oxide'.",
-},
-{
-  pattern: `<span style="color: blue;font-weight: 700;">MO</span>1ce<span style="color: blue;font-weight: 700;">ON</span>`,
-  answer: `Once in a blue moon`,
-  hint: `very rarely.`,
-},
-{
-  pattern: `&#x1F525 + &#x1F98a`,
-  answer: `Firefox`,
-  hint: `A popular Web browser.`,
-},
-{
-  pattern: `<h6>E</h6><h6>C</h6><h5>O</h5><h4>N</h4><h4>O</h4><h3>M</h3><h2>Y</h2>`,
-  answer: `Growing economy`,
-  hint: `The way the letters are becoming bigger matters.`,
-},
-{
-  pattern: `&#128068 + &#x1F962`,
-  answer: `Lipstick`,
-  hint: `a tool for make-up artistry`,
-},
-{
-  pattern: `&#11088 + &#x1F40B`,
-  answer: `Starfish`,
-  hint: `Say the two emojis out loud.`,
-},
-{
-  pattern: `&#x1F408 + as + (&#127942 -y + e) `,
-  answer: `Catastrophe`,
-  hint: `A really really bad occurence`,
-},
-{
-  pattern: `&#x1F41D + &#127810`,
-  answer: `Belief`,
-  hint: `Hmm, Say both emojis out loud`,
-},
-{
-  pattern: `stan4ce`,
-  answer: `For instance`,
-  hint: `as an example`,
-},
-{
-  pattern: `&#127769 + &#128161`,
-  answer: `Moonlight`,
-  hint: `Lights up the night`,
-},
-// {
-//   pattern: ``,
-//   answer: ``,
-//   hint: ``,
-// },
-{
-  pattern: `Mil1lion`,
-  answer: `1 in a million`,
-  hint: `Make sure you pay attention to the number included in the word.`,
-},
-{
-  pattern: `&#128065  &#128151  U`,
-  answer:" i love you",
-  hint: " An affirmation of affection or deep caring, especially to a family member. ",
-},
-{
-  pattern: '&#128161 + &#127968',
-  answer: 'Lighthouse',
-  hint: 'A tower with a light to guide ships at sea',
-},
-// ****************************************************
-{
-  pattern: `<h5><sup>Travel</sup></h5> <h2>/</h2> <h5><sub>ccccc</sub></h6> `,
-  answer:" Travel Oversea",
-  hint: " Beyond the boundaries of one's country ",
-},
-]
+  {
+    pattern: `improp + 2`,
+    answer: `impromptu`,
+    hint: `unprepared`,
+  },
+  {
+    pattern: `&#128514 + gas`,
+    answer: " laughing gas ",
+    hint: "A popular name for 'Nitrous Oxide'.",
+  },
+  {
+    pattern: `<span style="color: blue;font-weight: 700;">MO</span>1ce<span style="color: blue;font-weight: 700;">ON</span>`,
+    answer: `Once in a blue moon`,
+    hint: `very rarely.`,
+  },
+  {
+    pattern: `&#x1F525 + &#x1F98a`,
+    answer: `Firefox`,
+    hint: `A popular Web browser.`,
+  },
+  {
+    pattern: `<h6>E</h6><h6>C</h6><h5>O</h5><h4>N</h4><h4>O</h4><h3>M</h3><h2>Y</h2>`,
+    answer: `Growing economy`,
+    hint: `The way the letters are becoming bigger matters.`,
+  },
+  {
+    pattern: `&#128068 + &#x1F962`,
+    answer: `Lipstick`,
+    hint: `a tool for make-up artistry`,
+  },
+  {
+    pattern: `&#11088 + &#x1F40B`,
+    answer: `Starfish`,
+    hint: `Say the two emojis out loud.`,
+  },
+  {
+    pattern: `&#x1F408 + as + (&#127942 -y + e) `,
+    answer: `Catastrophe`,
+    hint: `A really really bad occurence`,
+  },
+  {
+    pattern: `&#x1F41D + &#127810`,
+    answer: `Belief`,
+    hint: `Hmm, Say both emojis out loud`,
+  },
+  {
+    pattern: `stan4ce`,
+    answer: `For instance`,
+    hint: `as an example`,
+  },
+  {
+    pattern: `&#127769 + &#128161`,
+    answer: `Moonlight`,
+    hint: `Lights up the night`,
+  },
+  // {
+  //   pattern: ``,
+  //   answer: ``,
+  //   hint: ``,
+  // },
+  {
+    pattern: `<p>stand</p><p>I</p>`,
+    answer: ` I understand`,
+    hint: `The placement of each item is important in order to solve this rebus puzzle.`,
+  },
+  {
+    pattern: `Mil1lion`,
+    answer: `1 in a million`,
+    hint: `Make sure you pay attention to the number included in the word.`,
+  },
+  {
+    pattern: `&#128065  &#128151  U`,
+    answer: " i love you",
+    hint: " An affirmation of affection or deep caring, especially to a family member. ",
+  },
+  {
+    pattern: "&#128161 + &#127968",
+    answer: "Lighthouse",
+    hint: "A tower with a light to guide ships at sea",
+  },
+  // ****************************************************
+  {
+    pattern: `<h5><sup>Travel</sup></h5> <h2>/</h2> <h5><sub>ccccc</sub></h6> `,
+    answer: " Travel Oversea",
+    hint: " Beyond the boundaries of one's country ",
+  },
+];
 
 /** Don't edit this**/
 export default rebuses;
